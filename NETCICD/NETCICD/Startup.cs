e6a61b14397fa4b85ef57d53c0045e771808ddaa
nghieppv <nghieppv@vnpt.vn>
2020-08-12 09:02:19 +0700

    Init project

---
+++ NETCICD/NETCICD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NETCICD.Startup))]
+namespace NETCICD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
